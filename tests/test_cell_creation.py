7481648491528eab80017305ed45de2d4e77a750
alabra26 <alexanderlabranche@gmail.com>
2025-03-18 01:56:01 -0400

    ci: initial commit

---
+++ tests/test_cell_creation.py
@@ -0,0 +1,10 @@
+import nbformat as nbf
+
+from utils import *
+
+def test_create_cell():
+    # load expected output Jupyter notebook
+
+    # create new Jupyter notebook from cell configuration
+
+    # assert equal
