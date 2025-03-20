d22c916738cee8b3843d1fbde3e9a7ec6a8da819
alabra26 <alexanderlabranche@gmail.com>
2025-03-19 22:06:58 -0400

    feat: add functions for scope tracking

--- bookmill/utils.py
+++ bookmill/utils.py
@@ -3,9 +3,9 @@ import papermill
 from Chapter import Chapter
 
 def create_cell_from_dict(celldata: dict) -> nbf.NotebookNode:
-    if celldata['celltype'] == 'code':
+    if celldata['cell_type'] == 'code':
         newcell = nbf.v4.new_code_cell(source = celldata['source'])
-    elif celldata['celltype'] == 'markdown':
+    elif celldata['cell_type'] == 'markdown':
         newcell = nbf.v4.new_markdown_cell(source = celldata['source'])
     newcell['tags'] = celldata['tags']
 
@@ -21,3 +21,26 @@ def add_chapter_to_notebook(nb: nbf.NotebookNode, chaptercells: list[dict]) -> n
         nb = add_cell_to_notebook(nb, celldata)
     nb = papermill.execute_notebook(nb)
     return nb
+
+def get_nb_scope(nb: nbf.NotebookNode):
+    # define variable scope cell
+    # TODO : move this to a config file
+    # TODO : make this a Chapter object
+    scope_cell = {
+        'cell_type': 'code',
+        'source': 'locals()',
+        'tags': ['scope', 'trim']
+    }
+    nb = add_cell_to_notebook(nb, scope_cell)
+    # TODO : determine the json structure to access the outputs
+    scope = nb['cells'][-1]['outputs']['data']['text/plain'].split()
+    return scope
+    
+
+def get_scope_additions(nb: nbf.NotebookNode, chaptercells: list[dict]) -> list[str]:
+    initial_scope = get_nb_scope(nb)
+    nb = add_chapter_to_notebook(nb, chaptercells)
+    final_scope = get_nb_scope(nb)
+    scopediff = set(final_scope) - set(initial_scope)
+    # return
+    return scopediff
